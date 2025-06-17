9648ca5ce5b2ab40737b7706d9ed03761c457c1f
SKIZHERE <kumarsaksham67@gmail.com>
2025-06-17 20:04:16 +0530

    sorting them

--- 2_arrays_soln/Q1/2_soln.py
+++
@@ -1,7 +0,0 @@
-# 2. Find out your total expense in first quarter (first three months) of the year.
-
-d = {"January":2200,"February":2350,"March":2600,"April":2130,"May":2190 }
-fin = sum(list(d.values())[0:3])
-print(f"total expense of first quarter is {fin}")
-
-
--- 2_arrays_soln/Q1/3_soln.py
+++
@@ -1,4 +0,0 @@
-# 3. Find out if you spent exactly 2000 dollars in any month
-d = {"January":2200,"February":2350,"March":2600,"April":2130,"May":2190 }
-k = list(i for i,val in d.items() if val == 2000)
-print(k)

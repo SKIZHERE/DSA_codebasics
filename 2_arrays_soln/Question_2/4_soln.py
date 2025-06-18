33423a07cf7f7fd9a4ef590fc4baea35a23f9e96
SKIZHERE <kumarsaksham67@gmail.com>
2025-06-18 13:34:15 +0530

    question 4 solved

---
+++ 2_arrays_soln/Question_2/4_soln.py
@@ -0,0 +1,9 @@
+# 4. Now you don't like thor and hulk because they get angry easily :)
+#    So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
+#    Do that with one line of code.
+
+heros = ['spider man', 'thor', 'hulk', 'black panther', 'iron man', 'captain america']
+
+heros[1:3] = ['doctor strange']
+
+print(heros)

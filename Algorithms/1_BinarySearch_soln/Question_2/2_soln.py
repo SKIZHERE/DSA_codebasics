0cf526358b0eacdbdb2d02d0b23ba9c50b803d85
SKIZHERE <kumarsaksham67@gmail.com>
2026-01-08 09:34:14 +0530

    added ques

--- Algorithms/1_BinarySearch_soln/Question_2/2_soln.py
+++ Algorithms/1_BinarySearch_soln/Question_2/2_soln.py
@@ -1,3 +1,11 @@
+"""
+Find index of all the occurances of a number from sorted list
+
+numbers = [1,4,6,9,11,15,15,15,17,21,34,34,56]
+number_to_find = 15
+This should return 5,6,7 as indices containing number 15 in the array
+"""
+
 def linear_search(elements, target):
     for i, element in enumerate(elements):
         if element == target:
